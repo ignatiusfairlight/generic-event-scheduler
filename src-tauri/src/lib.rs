6dd1816496ad402dae234d3513157ae9797e085a
Ignatius Fairlight <lawaierang@gmail.com>
2026-08-31 12:26:23 +0800

    Updated the crash logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,8 +12,16 @@ use commands::delete_event;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     std::panic::set_hook(Box::new(|info| {
-        let msg = format!("{:?}", info);
-        let _ = std::fs::write("crash_log.txt", msg);
+        let msg = if let Some(s) = info.payload().downcast_ref::<&str>() {
+            s.to_string()
+        } else if let Some(s) = info.payload().downcast_ref::<String>() {
+            s.clone()
+        } else {
+            "Unknown panic payload".to_string()
+        };
+        let location = info.location().map(|l| l.to_string()).unwrap_or_default();
+        let log = format!("Panic at {}: {}", location, msg);
+        let _ = std::fs::write("crash_log.txt", log);
     }));
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
